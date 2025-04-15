e291b3e7a89ed04fabc0f4806419d3868b33b6a6
nguyenthithuphuong1772k8 <109800073+nguyenthithuphuong1772k8@users.noreply.github.com>
2025-04-15 14:36:04 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="purchase-bot",
+    version="0.1",
+    install_requires=[
+        "streamlit==1.22.0",
+        "pandas==1.5.3",
+        "numpy==1.21.6",
+        "openpyxl==3.0.10"
+    ],
+)
